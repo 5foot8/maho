58417cdeb6b3f77f72f65dadc20944365a164bc1
5foot8 <andrew.beckett@me.com>
2019-08-18 20:15:23 +0100

    Updated camera control for two cameras
    The new cli has the ability to set a minimum height for aircraft.  This also now allows two cameras to face north and south and aircraft are passed-off to the other camera when they cross the determined latitude.  I've also included the following key presses - 's' saves a screen-shot, 'r' - restarts the camera (use if camera stops moving or doesn't appear to be following.)  Added a basic compass rose to the bottom of the screen.

--- maho/cli.py
+++ maho/cli.py
@@ -1,23 +1,54 @@
+#!/usr/bin/python
+
 import argparse
 import multiprocessing
 import queue
 import time
+import sqlite3
+import threading
+
+import cv2
+import subprocess
+import numpy as np
+import pylab
+
+import os
+import sys
+import math
+
+# import datetime
 
 from maho.camera import IPCamera
 from maho.adsb import Dump1090
 from maho.util import AzimuthAltitudeDistance
 
-import numpy as np
-import cv2
+from time import sleep
+
+
+def restart_program():
+     
+    print("Restarting")
+    time.sleep(0)
+    python = sys.executable
+    os.execl(sys.executable, 'python', __file__, *sys.argv[1:])
+    
+find_aircraft_icao = ("")
+window_name = ("Maho")
+
+def find_aircraft():
+    find_aircraft_icao = input("Which aircraft ICAO are you looking for? ")
+    
+
+#conn = sqlite3.connect('planeDB.db')
+#print ("Opened database successfully");
 
 
 def camera_control(camera_host, camera_port, camera_user, camera_pass, q):
     """Control a maho.Camera based on inputs from a multiprocessing queue"
 
-        On startup this function will place the stream_url in the queue if camera
-        communication works.
-        If it fails the exception for why will be placed in the queue before exiting
-    """
+        On startup this function will place the stream_url in the queue if
+        camera communication works. If it fails the exception for why will be
+        placed in the queue before exiting """
 
     try:
         camera = IPCamera(camera_host, camera_port, camera_user, camera_pass)
@@ -30,6 +61,10 @@ def camera_control(camera_host, camera_port, camera_user, camera_pass, q):
             camera.move_to(*q.get())
     except KeyboardInterrupt:
         pass
+    
+    
+        
+            
 
 
 def track_closest_aircraft(latitude, longitude, elevation, host, port, q):
@@ -40,9 +75,11 @@ def track_closest_aircraft(latitude, longitude, elevation, host, port, q):
         port (int): The dump1090 port
         q (queue): Messages will be placed in this queue
 
-        On startup this function will place True in the queue if dump1090 starts properly
-        If it fails the exception will be placed in the queue before exiting
+        On startup this function will place True in the queue if dump1090
+        starts properly. If it fails the exception will be placed in the
+        queue before exit
     """
+    
     try:
         d = Dump1090(host, port)
         q.put(True)
@@ -52,7 +89,12 @@ def track_closest_aircraft(latitude, longitude, elevation, host, port, q):
 
     target = None
     target_distance = None
+    last_azi = None
+    
+
+
     aad = AzimuthAltitudeDistance(latitude, longitude, elevation)
+    
 
     try:
         for aircraft in d.updates():
@@ -62,18 +104,100 @@ def track_closest_aircraft(latitude, longitude, elevation, host, port, q):
                 lng,
                 aircraft.altitude
             )
+            
+
+            if aircraft.position[0] > latitude:
+                northSouth = ('N')
+            else:
+                northSouth = ('S')
+                
+            if lat > latitude:
+                northSouth_t = ('N')
+            else:
+                northSouth_t = ('S')                
+            
 
             # if we don't have a target we do now
             # or target is old, then use this new aircraft
             # or new aircraft isn't the target, but it is closer, so we switch!
-            if (target is None or target.age > 60 or
-                    (target.icao != aircraft.icao and distance < target_distance)):
+
+            # print (("<--{}-->{}km {} {}").format(aircraft,int((distance)/1000), aircraft.icao, time.time()))
+                        
+            # make sure aircraft swaps between cameras as it passes over
+            # so the correct camera is pointing at the aircraft, if this is not
+            # here the camera continues to follow untill an aircraft is closer.
+            
+            # if you are using only one camera you dont require this section
+            
+            my_lat = 53.43450
+            
+            '''if find_aircraft_icao = aircraft.icao:
                 target = aircraft
+                print (find_aircraft_icao)
+            else:
+                pass'''
+            
+            
+            if latitude >= my_lat and target is None:
+                pass
+            elif (latitude >= my_lat and  target.icao == aircraft.icao and lat < my_lat):
+                target = None
+                #q.put((target, azimuth, altitude, distance))
+            else:
+                pass
+            
+            if latitude <= my_lat and target is None:
+                pass
+            elif (latitude <= my_lat and  target.icao == aircraft.icao and lat > my_lat):
+                target = None
+                #q.put((target, azimuth, altitude, distance))
+            else:
+                pass
+            
+            '''try
+            while find_aircraft_icao == aircraft.icao:
+                target = aircraft
+                print (find_aircraft_icao)
+            else:
+                pass'''
+            
+            
+            
+            
+            
+            # Mondified code to split between the North and South facing cameras.
+            
+            #North facing camera:
+            if (latitude >= my_lat) and (aircraft.altitude >= (30000 * 0.3048)) or (aircraft.position[0] < latitude):
+            
+                if (target is None or target.age > 10 or target.icao != aircraft.icao and distance < target_distance) \
+                    and northSouth is ('N'):
+                    #print (distance, target_distance)
+                    #print (lat-latitude)
+                    target = aircraft
+                else:
+                    pass
+                
+            #South facing camera:
+            
+            if (latitude <= my_lat) and (aircraft.altitude >= (30000 * 0.3048)) or (aircraft.position[0] > latitude):
+            
+                if (target is None or target.age > 10 or target.icao != aircraft.icao and distance < target_distance) \
+                    and northSouth is ('S'):
+                    target = aircraft
+                else:
+                    pass
+                
+            else:
+                pass
+            
+            #print (find_active, " ", find_aircraft_icao)
 
             # if we aren't the target at this point then bail
-            if target.icao != aircraft.icao:
+            
+            if target != aircraft:
                 continue
-
+            
             target = aircraft
             target_distance = distance
             q.put((target, azimuth, altitude, distance))
@@ -93,7 +217,9 @@ def go_maho(
     adsb_port,
 ):
 
-    # fork a process to communicate with dump1090
+
+    
+   # fork a process to communicate with dump1090
     targets = multiprocessing.Queue()
     tracker = multiprocessing.Process(
         target=track_closest_aircraft,
@@ -112,12 +238,14 @@ def go_maho(
             status
         ))
 
-    # run camera control in own process as moving the camera can block for seconds
+    # run camera control in own process as moving the camera
+    # can block for seconds
     camera_queue = multiprocessing.Queue()
     camera = multiprocessing.Process(
         target=camera_control,
         args=(camera_host, camera_port, camera_user, camera_pass, camera_queue,)
     )
+    # myCamera = args.camera_host
     camera.start()
 
     # fist thing in the queue will be startup status
@@ -134,13 +262,13 @@ def go_maho(
     cap = cv2.VideoCapture(stream_url)
     ret, frame = cap.read()
 
-    cv2.namedWindow("maho")
+    # cv2.namedWindow("maho")
 
-    orb = cv2.ORB_create()
+    # orb = cv2.ORB_create()
 
     # build a mask that's the center of the frame
     # we'll focus searching for aircraft in this region
-    search_mask = np.zeros((frame.shape[0], frame.shape[1], 1), dtype=np.uint8)
+    # search_mask = np.zeros((frame.shape[0], frame.shape[1], 1), dtype=np.uint8)
     cx = frame.shape[1] / 2
     cy = frame.shape[0] / 2
 
@@ -159,19 +287,18 @@ def go_maho(
 
     target = None
     last_target = None
+    last_azi = 0
+    last_aztime = 0
+    # distanceOld = 0
+    # photoTaken = 0
     try:
         while True:
             start = time.time()
 
-            # fill our mask back to full, we may have chopped it smaller on the last frame
-            search_mask[
-                search_rect[0][0]:search_rect[0][1],
-                search_rect[1][0]:search_rect[1][1]
-            ] = 255
-
+            
             # grab a frame from the camera
             ret, frame = cap.read()
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             # check for new / updated target info
             try:
@@ -191,8 +318,35 @@ def go_maho(
                     altitude,
                     int(distance)
                 ))
-
-                camera_queue.put((azimuth, altitude))
+                
+                # a function to slow down large camera movements and also
+                # keep the camera moving when large gaps between reading ASDB data
+                # from the aeroplane.
+ 
+                aztime = time.time()
+                angle_move = (last_azi - azimuth)
+                time_between = (aztime - last_aztime)
+                angle_move_rate = (angle_move / time_between)
+                
+                
+                if (abs(angle_move_rate)) > 15:
+                    angle_move_rate = (angle_move_rate / 2)
+                else:
+                    pass
+                
+                if time_between == 0:
+                    pass
+                else:
+                    #print (angle_move_rate," Angle move")
+                    pass
+                
+                last_aztime = aztime
+                last_azi = azimuth
+                
+                sleep(1)
+                camera_queue.put(((azimuth + angle_move_rate), altitude))
+                
+                
             except queue.Empty:
                 pass
 
@@ -203,12 +357,12 @@ def go_maho(
                     target.callsign or target.icao,
                     (0, 50),
                     cv2.FONT_HERSHEY_DUPLEX,
-                    2,
+                    1.5,
                     (255, 255, 255),
-                    4,
+                    2,
                     cv2.LINE_AA
                 )
-
+                
                 txt = "{0:.3f}, {1:.3f} @ {2:.0f}m (dist: {3:.0f}m)".format(
                     target.position[0],
                     target.position[1],
@@ -226,11 +380,88 @@ def go_maho(
                     cv2.LINE_AA
                 )
 
-                cv2.rectangle(frame, search_rect[0][::-1], search_rect[1][::-1], (0, 0, 255), 2)
+                cv2.rectangle(frame, search_rect[0][::-1], search_rect[1][::-1]
+                    , (0, 0, 255), 2)
 
-                kp = orb.detect(gray, search_mask)
-                kp, des = orb.compute(gray, kp)
-                cv2.drawKeypoints(frame, kp, frame, color=(0, 255, 0), flags=0)
+                # draw a line in the frame at the south point
+                # the left edge is always 45 degrees less than camera angle
+                # right edge is 45 degrees more than azimuth angle.
+
+                if frame.any() is None:
+                    bx = 1
+                    by = 1
+                else:
+                    bx = int(frame.shape[1])
+                    by = int(frame.shape[0])
+                
+                # fov = ((azimuth - 45),(azimuth + 45))  #field of view
+                
+                findSouth = ((45 - int(azimuth - 180)))
+                findWest = ((45 - int(azimuth - 270)))
+                findEast = ((45 - int(azimuth - 90)))
+                
+                if findSouth >= 180:
+                    findNorth = ((findSouth - 180))
+                else:
+                    findNorth = ((findSouth + 180))
+                    
+                southLine = int((findSouth / 90) * bx)
+                westLine = int((findWest / 90) * bx)
+                eastLine = int((findEast / 90) * bx)
+                northLine = int((findNorth / 90) * bx)
+                
+                cv2.line(frame, (southLine, (by-100)),
+                (southLine, by), (0, 0, 255), 2)
+                cv2.line(frame, (westLine, (by-100)),
+                (westLine, by), (0, 255, 0), 2)
+                cv2.line(frame, (eastLine, (by-100)),
+                (eastLine, by), (0, 255, 255), 2)
+                cv2.line(frame, (northLine, (by-100)),
+                (northLine, by), (255, 255, 255), 2)
+
+                cv2.putText(
+                    frame,
+                    "S",
+                    ((southLine + 5), (by-80)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0, 0, 255),
+                    1,
+                    cv2.LINE_AA
+                )
+
+                cv2.putText(
+                    frame,
+                    "W",
+                    ((westLine + 5), (by-80)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0, 255, 0),
+                    1,
+                    cv2.LINE_AA
+                )
+
+                cv2.putText(
+                    frame,
+                    "E",
+                    ((eastLine + 5), (by-80)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0, 255, 255),
+                    1,
+                    cv2.LINE_AA
+                )
+
+                cv2.putText(
+                    frame,
+                    "N",
+                    ((northLine + 5), (by-80)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 255),
+                    1,
+                    cv2.LINE_AA
+                )
 
                 cv2.putText(
                     frame,
@@ -253,9 +484,33 @@ def go_maho(
                     cv2.LINE_AA
                 )
 
+            
+            # different ways of displaying the final output.
+            # uncomment to activate, not sure if you cannot have more than three active.
+            
+            my_lat = 53.43450 
+            
+            if latitude >= my_lat:
+                window_name = ('Maho NORTH')
+            else:
+                window_name = ('Maho SOUTH')
+            
             # display it
-            cv2.imshow('maho', frame)
-
+            # small = cv2.resize(frame, (0,0), fx=1, fy=1)
+            # small = frame
+            # small = cv2.resize(frame, (0,0), width, height)
+            # cv2.imshow('maho', frame)
+            cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+            # cv2.setWindowProperty('Maho', cv2.WINDOW_FULLSCREEN)
+            # cv2.setWindowProperty('Maho', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+            # cv2.setWindowProperty('Maho', cv2.WINDOW_AUTOSIZE, cv2.WINDOW_FULLSCREEN)
+            cv2.setWindowProperty(window_name, cv2.WINDOW_AUTOSIZE, cv2.WND_PROP_FULLSCREEN)
+            # cv2.namedWindow('Maho', cv2.WND_PROP_FULLSCREEN)
+            # cv2.resizeWindow('Maho', 1000, 700)
+            cv2.imshow(window_name, frame)
+            
+            
+            
             # handle input
             keypress = cv2.waitKey(1) & 0xFF
 
@@ -265,6 +520,31 @@ def go_maho(
 
             if keypress == ord('q'):
                 raise KeyboardInterrupt
+
+            # Create a timestamp
+
+            # timestr = time.strftime("%Y%m%d-%H%M%S")
+
+            # Press 's' to save an image
+            # Image saves with timestamp as filename
+            # and a note is added to the script window to show
+            # that the file has been created.
+
+            def saveScreen():
+                timestr = time.strftime("%Y%m%d-%H%M%S")
+                cv2.imwrite('MC' + timestr + '-' + target.icao + '.png', frame)
+                # cv2.imwrite('MC' + timestr + '.png', frame)
+                print("Image saved MC" + timestr)
+
+            if keypress == ord('s'):
+                saveScreen()
+                
+            if keypress == ord('r'):
+                restart_program()
+            
+            if keypress == ord('f'):
+                find_aircraft()
+
 
     except KeyboardInterrupt:
         tracker.terminate()
@@ -280,21 +560,30 @@ def go_maho(
 def main():
     parser = argparse.ArgumentParser(
         prog='maho',
-        description='ADS-B asdisted aircraft spotting',
+        description='ADS-B assisted aircraft spotting',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument('--latitude', type=float, required=True, help='Latitude of the camera')
-    parser.add_argument('--longitude', type=float, required=True, help='Longitude of the camera')
-    parser.add_argument('--elevation', type=float, required=True, help='Elevation of the camera')
+    parser.add_argument('--latitude', type=float, required=True,
+    help='Latitude of the camera')
+    parser.add_argument('--longitude', type=float, required=True,
+    help='Longitude of the camera')
+    parser.add_argument('--elevation', type=float, required=True,
+    help='Elevation of the camera')
 
-    parser.add_argument('--camera-host', type=str, required=True, help='Camera hostname/ip')
-    parser.add_argument('--camera-port', type=int, default=80, help='Camera port')
+    parser.add_argument('--camera-host', type=str, required=True,
+    help='Camera hostname/ip')
+    parser.add_argument('--camera-port', type=int, default=80,
+    help='Camera port')
 
-    parser.add_argument('--camera-user', type=str, required=True, help='Camera username')
-    parser.add_argument('--camera-pass', type=str, required=True, help='Camera password')
+    parser.add_argument('--camera-user', type=str, required=True,
+    help='Camera username')
+    parser.add_argument('--camera-pass', type=str, required=True,
+    help='Camera password')
 
-    parser.add_argument('--adsb-host', type=str, default='localhost', help='dump1090 hostname/ip')
-    parser.add_argument('--adsb-port', type=int, default=30002, help='dump1090 TCP raw output port')
+    parser.add_argument('--adsb-host', type=str, default='localhost',
+    help='dump1090 hostname/ip')
+    parser.add_argument('--adsb-port', type=int, default=30002,
+    help='dump1090 TCP raw output port')
 
     args = parser.parse_args()
 
@@ -310,6 +599,9 @@ def main():
             args.adsb_host,
             args.adsb_port
         )
+
+        # myCamera = (args.camera_host)
+
     except KeyboardInterrupt:
         pass
     except RuntimeError as exc:
@@ -319,3 +611,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
